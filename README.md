e7630ed9a4226359d4d4b05273ee5fcc529a612e
Kiana-Jahanshid <tannaz.maleki82@gmail.com>
2024-05-24 02:47:41 +0330

    setup & readme edited

--- README.md
+++ README.md
@@ -3,14 +3,18 @@
 
 ## Installation 
 
+```bash
     pip install picopy
-
+```
 
 ## How to use 
+
 Add your image address in below command : 
 
+```python
     from picopy import picopy 
 
     picopy.convert_image_to_watercolor("test.jpg") 
+```
 
 Your watercolored image will be saved in your file folder .
--- setup.py
+++ setup.py
@@ -2,21 +2,30 @@
 
 # this setuptools package help us to install other package 
 from setuptools import setup
+from pathlib import Path
 
 
 def pre_install():
-    f = open("README.md" , "r")
-    text = f.read()
-    return text
+    #f = open("README.md" , "r")
+    #text = f.read()
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()    
+    return long_description
+
+with open('./requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 
 setup(name="picopy" ,
-      version="1.0.5", 
-      author="kiki js" , 
-      description="convert an image to a watercolor image 🎨", 
-      long_description=pre_install() , # its like readme
-      requires= [] ,
-      author_email="k.jhnshid@gmail.com",
-      packages=["picopy"]
-      )
+    version="1.0.6", 
+    author="kiki js" , 
+    description="convert an image to a watercolor image 🎨", 
+    long_description=pre_install() ,
+    requires= [] ,
+    author_email="k.jhnshid@gmail.com",
+    packages=["picopy"] ,
+    long_description_content_type='text/markdown' ,
+    install_requires= install_requires
+    )
 
 
