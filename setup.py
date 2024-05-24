433a8b65ff3d4d49d6780928aaaf2d46c407ef58
Kiana-Jahanshid <tannaz.maleki82@gmail.com>
2024-05-24 10:54:57 +0330

    version edited

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('./requirements.txt') as f:
 
 
 setup(name="picopy" ,
-    version="1.0.11", 
+    version="1.0.12", 
     author="kiki js" , 
     description="convert an image to a watercolor image 🎨", 
     long_description=pre_install() ,
